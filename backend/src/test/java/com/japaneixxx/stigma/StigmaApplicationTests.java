cc30acab1996f776e58fc1befb234d3ca4daa6e2
Japaneixxx <Japaneix@gmail.com>
2026-03-27 16:44:27 -0300

    commit

---
+++ backend/src/test/java/com/japaneixxx/stigma/StigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japaneixxx.stigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StigmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
